55784d329cd4285b3c99b34f0d0504a91fc56ff6
Martina Kurkova <martinanoha@seznam.cz>
2025-04-08 14:43:42 +0200

    project_29

--- 29_double-click-heart/script.js
+++ 29_double-click-heart/script.js
@@ -1,12 +1,13 @@
-const loveMe = document.querySelector('.loveMe')
-const times = document.querySelector('#times')
+const loveMe = document.querySelector(".loveMe")
+const times = document.querySelector("#times")
 
 let clickTime = 0
 let timesClicked = 0
 
-loveMe.addEventListener('click', (e) => {
+loveMe.addEventListener("click", (e) => {
     if(clickTime === 0) {
         clickTime = new Date().getTime()
+        console.log(clickTime)
     } else {
         if((new Date().getTime() - clickTime) < 800) {
             createHeart(e)
@@ -18,9 +19,9 @@ loveMe.addEventListener('click', (e) => {
 })
 
 const createHeart = (e) => {
-    const heart = document.createElement('i')
-    heart.classList.add('fas')
-    heart.classList.add('fa-heart')
+    const heart = document.createElement("i")
+    heart.classList.add("fas")
+    heart.classList.add("fa-heart")
 
     const x = e.clientX
     const y = e.clientY
