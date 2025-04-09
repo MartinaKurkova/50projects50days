d34c6696fe21d3b337d93e7791c2bfc9f9295d0b
Martina Kurkova <martinanoha@seznam.cz>
2025-04-09 09:35:57 +0200

    project_30

--- 30_auto-text-effect/script.js
+++ 30_auto-text-effect/script.js
@@ -1,10 +1,10 @@
-const textEl = document.getElementById('text')
-const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
-let idx = 1
-let speed = 300 / speedEl.value
+const textEl = document.getElementById("text");
+const speedEl = document.getElementById("speed");
+const text = `We love Programming`;
+let idx = 1;
+let speed = 300 / speedEl.value;
 
-writeText()
+writeText();
 
 function writeText() {
     textEl.innerText = text.slice(0, idx)
@@ -12,11 +12,10 @@ function writeText() {
     idx++
 
     if(idx > text.length) {
-        idx = 1
+        idx  = 1
     }
 
     setTimeout(writeText, speed)
 }
 
-
-speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
+speedEl.addEventListener("input", (e) => speed = 300 / e.target.value)
