792c3663d8b091eca2f130ed34f822797cb12bb4
Martina Kurkova <martinanoha@seznam.cz>
2025-04-17 14:49:09 +0200

    Project_37

--- 37_pokedex/script.js
+++ 37_pokedex/script.js
@@ -1,5 +1,5 @@
-const poke_container = document.getElementById('poke-container')
-const pokemon_count = 150
+const poke_container = document.getElementById("poke-container");
+const pokemon_count = 150;
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -19,25 +19,25 @@ const colors = {
 
 const main_types = Object.keys(colors)
 
-const fetchPokemons = async () => {
+const fetchPokemons = async() => {
     for(let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i)
     }
 }
 
-const getPokemon = async (id) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+const getPokemon = async(id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}` 
     const res = await fetch(url)
     const data = await res.json()
     createPokemonCard(data)
 }
 
 const createPokemonCard = (pokemon) => {
-    const pokemonEl = document.createElement('div')
-    pokemonEl.classList.add('pokemon')
+    const pokemonEl = document.createElement("div")
+    pokemonEl.classList.add("pokemon")
 
     const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
-    const id = pokemon.id.toString().padStart(3, '0')
+    const id = pokemon.id.toString().padStart(3, "0")
 
     const poke_types = pokemon.types.map(type => type.type.name)
     const type = main_types.find(type => poke_types.indexOf(type) > -1)
@@ -54,7 +54,7 @@ const createPokemonCard = (pokemon) => {
         <h3 class="name">${name}</h3>
         <small class="type">Type: <span>${type}</span> </small>
     </div>
-    `
+    ` 
 
     pokemonEl.innerHTML = pokemonInnerHTML
 
@@ -62,3 +62,9 @@ const createPokemonCard = (pokemon) => {
 }
 
 fetchPokemons()
+
+    
+   
+
+
+
